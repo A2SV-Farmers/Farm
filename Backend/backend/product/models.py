58e595f1a458ac4107667be6e4cb6520874ae7a1
Missoum Youcef <missoumleetcoding@gmail.com>
2023-09-02 12:10:33 -0500

    create product

--- Backend/backend/product/models.py
+++ Backend/backend/product/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='products/')
+    reviews = models.JSONField(default=list)
+    rating = models.DecimalField(max_digits=3, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
